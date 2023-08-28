1f6736265d2277cfbce2f4f34c89dec1be5f42ef
Dicky Micky <XiaolongGao521>
2023-08-27 20:37:16 -0700

    End of random graphics

--- RandomGraphics/main.js
+++ RandomGraphics/main.js
@@ -1,23 +1,41 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
-camera.position.set( 0, 0, 100 );
-camera.lookAt( 0, 0, 0 );
-
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const scene = new THREE.Scene();
+const loader = new GLTFLoader();
 
-const points = [];
-points.push( new THREE.Vector3( - 10, 0, 0 ) );
-points.push( new THREE.Vector3( 0, 10, 0 ) );
-points.push( new THREE.Vector3( 10, 0, 0 ) );
+loader.load( 'models/megumin.glb', function ( gltf ) {
 
-const geometry = new THREE.BufferGeometry().setFromPoints( points );
-const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
-const line = new THREE.Line( geometry, material );
-scene.add( line );
+	scene.add( gltf.scene );
 
-renderer.render( scene, camera );
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
+
+const zero = new THREE.Vector3();
+const offset = new THREE.Vector3();
+const distance = 150;
+const clock = new THREE.Clock();
+
+function animate() {
+	requestAnimationFrame( animate );
+
+    const time = clock.getElapsedTime();
+
+    offset.x = distance * Math.sin( time );
+    offset.z = distance * Math.cos( time );
+
+    camera.position.copy( zero ).add( offset );
+    camera.lookAt( zero );
+
+	renderer.render( scene, camera );
+}
+
+animate();
